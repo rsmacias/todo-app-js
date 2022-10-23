60b331d53f7f94c3b41e1ba51e47fd3756743f44
Robert Macias <rsmacias@gmail.com>
2022-10-23 15:42:54 -0500

    Adding first look of validation form method

---
+++ main.js
@@ -0,0 +1,22 @@
+
+let form = document.getElementById("form");
+let input = document.getElementById("input");
+let msg = document.getElementById("msg");
+let posts = document.getElementById("posts");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log("button clicked");
+
+    formValidation();
+});
+
+let formValidation = () => {
+    if (!input || input.value === "") {
+        msg.innerHTML = "Post cannot be blank";
+        console.log("failure");
+    } else {
+        console.log("success");
+        msg.innerHTML = "";
+    }
+};
